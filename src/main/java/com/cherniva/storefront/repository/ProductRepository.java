eb00b656e2de54c130df0cd6845b34d96cfdbe14
Ivan Chernikov <chernikoviivan@gmail.com>
2025-05-11 18:02:14 +0200

    Add method to retrieve products with count greater than zero

--- src/main/java/com/cherniva/storefront/repository/ProductRepository.java
+++ src/main/java/com/cherniva/storefront/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.cherniva.storefront.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> getByName(String name);
+    @Query("SELECT p FROM Product p WHERE p.count IS NOT NULL AND p.count > 0")
+    List<Product> getProductsByCountGreaterThanZero();
     Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
     Page<Product> findByPriceBetween(Double minPrice, Double maxPrice, Pageable pageable);
 }
