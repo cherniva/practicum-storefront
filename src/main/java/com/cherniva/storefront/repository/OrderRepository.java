60bb04cd1fd262c76caafa49f4f08bd22557d4ca
Ivan Chernikov <chernikoviivan@gmail.com>
2025-05-11 16:14:09 +0200

    Change Order type to CustomerOrder after class renaming

--- src/main/java/com/cherniva/storefront/repository/OrderRepository.java
+++ src/main/java/com/cherniva/storefront/repository/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.cherniva.storefront.repository;
 
-import com.cherniva.storefront.model.Order;
+import com.cherniva.storefront.model.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<CustomerOrder, Long> {
 }
