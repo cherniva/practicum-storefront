c786e2d69e8d8bf81bc2b90000f00a8cd5163982
Ivan Chernikov <chernikoviivan@gmail.com>
2025-05-11 20:24:51 +0200

    Refactor: rename OrderRepository to CustomerOrderRepository

--- src/main/java/com/cherniva/storefront/repository/CustomerOrderRepository.java
+++ src/main/java/com/cherniva/storefront/repository/CustomerOrderRepository.java
@@ -3,5 +3,5 @@ package com.cherniva.storefront.repository;
 import com.cherniva.storefront.model.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<CustomerOrder, Long> {
+public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
 }
