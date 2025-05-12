a2515db425985d73756f7276f5fd5a85ac75b369
Ivan Chernikov <chernikoviivan@gmail.com>
2025-05-12 19:36:03 +0200

    Remove unused code

--- src/main/java/com/cherniva/storefront/repository/ProductRepository.java
+++ src/main/java/com/cherniva/storefront/repository/ProductRepository.java
@@ -15,5 +15,4 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p WHERE p.count IS NOT NULL AND p.count > 0")
     List<Product> getProductsByCountGreaterThanZero();
     Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
-    Page<Product> findByPriceBetween(Double minPrice, Double maxPrice, Pageable pageable);
 }
