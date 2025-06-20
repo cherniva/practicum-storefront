934703f4d4c6da30ed3f1c00f12c08e0b097429c
Ivan Chernikov <chernikoviivan@gmail.com>
2025-06-20 11:25:33 +0200

    Start implementing resource server

---
+++ payment/src/main/java/com/cherniva/payment/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.cherniva.payment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity security) throws Exception {
+        return security
+                .authorizeExchange(requests -> requests
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(serverSpec -> serverSpec
+                        .jwt(jwtSpec -> {
+                            ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+                            jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+                                List<String> roles = jwt.getClaim("roles");
+
+                                // Возвращаемый тип — Flux
+                                return Flux.fromIterable(roles)
+                                        .map(SimpleGrantedAuthority::new);
+                            });
+
+                            jwtSpec.jwtAuthenticationConverter(jwtAuthenticationConverter);
+                        })
+                )
+                .build();
+    }
+}
